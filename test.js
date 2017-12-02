677b31e4f91ac0ba0ae777df9ed5fd4180c65c78
Zuzanna Chrzastek <zuzanna.chrzastek@gmail.com>
2017-12-02 16:28:28 +0100

    Basic request to GitHub

--- test.js
+++ test.js
@@ -3,3 +3,20 @@ const cmdLineArgs = require('command-line-args');
 const cmdLineArgsConf = require('./cmdLineConf');
 const {login, password} = cmdLineArgs(cmdLineArgsConf);
 console.log(login, password);
+
+const request = require('request');
+
+const requestOptions = {
+    url: `https://${login}:${password}@api.github.com/search/repositories?sort=stars`
+};
+
+new Promise((resolve, reject) => {
+    return request(requestOptions, (error, response, body) => {
+        if(error) return reject(error);
+        return resolve(body);
+    });
+}).then(body => {
+    console.log(body);
+}).catch(error => {
+    console.log(error);
+});
