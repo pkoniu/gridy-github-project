44374c7f8f0e73d4d84f26aede18f4709ff326a5
Patryk Konior <patryk@arteia.co>
2017-12-02 20:27:27 +0100

    saving owners data to db

--- github-crawler.js
+++ github-crawler.js
@@ -23,73 +23,91 @@ const requestOptions = {
     json: true
 };
 
-MongoClient.connect(MONGO_URL)
-    .then(bigtableDB => {
-        console.log('Connected to bigtable-db in mongo.');
-        console.log('Requesting data from github');
-
-        db = bigtableDB;
-        return new Promise((resolve, reject) => {
-            return request(requestOptions, (error, response, body) => {
-                if (error) return reject(error);
-                return resolve(body);
-            });
-        });
-    }).then(body => {
-        console.log('Received reponse from github, saving to db.');
-        githubResponse = body;
-
-        const reposInfo = _.map(githubResponse.items, item => {
-            return {
-                name: item.name,
-                issues: item.open_issues_count,
-                size: item.size,
-                language: item.language,
-                stars: item.stargazers_count,
-                watchers: item.watchers_count,
-                score: item.score
-            };
-        });
-
-        return db.collection('repos').insertMany(reposInfo);
-    }).then(insertResult => {
-        console.log(`Inserted ${insertResult.insertedCount} repositories details.`)
-
-        const ownersInfo = _.chain(githubResponse.items).map(item => {
-            return {
-                login: item.owner.login,
-                url: item.owner.url,
-                type: item.owner.type
-            };
-        }).uniq().value();
-
-        return Promise.all(ownersInfo.map(ownerInfo => {
-            return new Promise((resolve, reject) => {
-                console.log(`Requesting addition info for: ${ownerInfo.login}`);
-                const requestDetails = {
-                    url: ownerInfo.url,
-                    headers: {
-                        'User-Agent': 'Gridy-Crawler'
-                    },
-                    json: true
-                };
-                return request(requestDetails, (error, response, body) => {
-                    if (error) return reject({
-                        error,
-                        ownerInfo
-                    });
-                    return resolve(_.merge(ownerInfo, {
-                        public_repos: body.public_repos,
-                        public_gists: body.public_gists,
-                        followers: body.followers,
-                        following: body.following,
-                        created_at: body.created_at
-                    }));
-                });
-            });
-        }));
-    }).then(ownersWithAdditionalInfo => {
-        debugger;
-    }).catch(error => {
+return MongoClient.connect(MONGO_URL)
+    .then(downloadDataFromGithub)
+    .then(saveReposDataToDB)
+    .then(requestAdditionalDataForOwners)
+    .then(saveOwnersDataToDB)
+    .then(insertResult => {
+        console.log(`Inserted ${insertResult.insertedCount} owners data.`);
+        return db.close();
+    })
+    .catch(error => {
         console.log(error);
+        return db.close();
     });
+
+function downloadDataFromGithub(bigtableDB) {
+    console.log('Connected to bigtable-db in mongo.');
+    console.log('Requesting data from github');
+
+    db = bigtableDB;
+    return new Promise((resolve, reject) => {
+        return request(requestOptions, (error, response, body) => {
+            if (error) return reject(error);
+            return resolve(body);
+        });
+    });
+};
+
+function saveReposDataToDB(body) {
+    console.log('Received reponse from github, saving to db.');
+    githubResponse = body;
+
+    const reposInfo = _.map(githubResponse.items, item => {
+        return {
+            name: item.name,
+            issues: item.open_issues_count,
+            size: item.size,
+            language: item.language,
+            stars: item.stargazers_count,
+            watchers: item.watchers_count,
+            score: item.score
+        };
+    });
+
+    return db.collection('repos').insertMany(reposInfo);
+};
+
+function requestAdditionalDataForOwners(insertResult) {
+    console.log(`Inserted ${insertResult.insertedCount} repositories details.`)
+
+    const ownersInfo = _.chain(githubResponse.items).map(item => {
+        return {
+            login: item.owner.login,
+            url: item.owner.url,
+            type: item.owner.type
+        };
+    }).uniq().value();
+
+    return Promise.all(ownersInfo.map(ownerInfo => {
+        return new Promise((resolve, reject) => {
+            console.log(`Requesting additional info for: ${ownerInfo.login}`);
+            const requestDetails = {
+                url: ownerInfo.url,
+                headers: {
+                    'User-Agent': 'Gridy-Crawler'
+                },
+                json: true
+            };
+            return request(requestDetails, (error, response, body) => {
+                if (error) return reject({
+                    error,
+                    ownerInfo
+                });
+                return resolve(_.merge(ownerInfo, {
+                    public_repos: body.public_repos,
+                    public_gists: body.public_gists,
+                    followers: body.followers,
+                    following: body.following,
+                    created_at: body.created_at
+                }));
+            });
+        });
+    }));
+};
+
+function saveOwnersDataToDB(ownersWithAdditionalInfo) {
+    console.log('Received data about owners');
+    return db.collection('owners').insertMany(ownersWithAdditionalInfo);
+};
