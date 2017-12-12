728153d4b87617f262100bb0b9f1e739977a5a0c
Zuzanna Chrzastek <zuzanna.chrzastek@gmail.com>
2017-12-13 00:57:40 +0100

    Creating column families

--- prepare-bigtable.js
+++ prepare-bigtable.js
@@ -9,6 +9,10 @@ const instance = bigtable.instance('bigtable-github');
 const tableRepos = instance.table('repos');
 const tableOwners = instance.table('owners');
 
+const familyReposGeneral = tableRepos.family('general');
+const familyReposSocial = tableRepos.family('social');
+const familyOwnersGeneral = tableOwners.family('general');
+
 
 new Promise((resolve, reject) => {
     return tableRepos.exists((error, exists) => {
@@ -37,5 +41,50 @@ new Promise((resolve, reject) => {
     return tableOwners.create();
 }).then(data => {
     const tableOwners = data[0];
+    const apiResponse = data[1];
+
+    return new Promise((resolve, reject) => {
+        return familyReposGeneral.exists((error, exists) => {
+            if(error) return reject(error);
+            return resolve(exists);
+        })
+    })
+}).then(familyReposGeneralExists => {
+    if(familyReposGeneralExists){
+        return Promise.resolve([]);
+    }
+    return familyReposGeneral.create();
+}).then(data => {
+    const family = data[0];
+    const apiResponse = data[1];
+
+    return new Promise((resolve, reject) => {
+        return familyReposSocial.exists((error, exists) => {
+            if(error) return reject(error);
+            return resolve(exists);
+        })
+    })
+}).then(familyReposSocialExists => {
+    if(familyReposSocialExists){
+        return Promise.resolve([]);
+    }
+    return familyReposSocial.create();
+}).then(data => {
+    const family = data[0];
+    const apiResponse = data[1];
+
+    return new Promise((resolve, reject) => {
+        return familyOwnersGeneral.exists((error, exists) => {
+            if(error) return reject(error);
+            return resolve(exists);
+        })
+    })
+}).then(familyOwnersGeneralExists => {
+    if(familyOwnersGeneralExists){
+        return Promise.resolve([]);
+    }
+    return familyOwnersGeneral.create();
+}).then(data => {
+    const family = data[0];
     const apiResponse = data[1];
 }).catch(console.log);
