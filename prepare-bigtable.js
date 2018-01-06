36c5f565f7a7d03453f3d7f106d0c55457b3ffb5
pkoniu <patryk.konior@gmail.com>
2018-01-06 19:26:35 +0100

    Saving data to bigtable (almost done)

--- prepare-bigtable.js
+++ prepare-bigtable.js
@@ -1,10 +1,10 @@
 const Bigtable = require('@google-cloud/bigtable');
 
 const bigtable = new Bigtable({
-    projectId: 'gridy-github-bigtable',
-    keyFilename: 'gridy-github-bigtable-59c836497151.json' 
+    projectId: 'gridy-github',
+    keyFilename: 'xD.json'
 });
-const instance = bigtable.instance('bigtable-github');
+const instance = bigtable.instance('gridy-github');
 
 const tableRepos = instance.table('repos');
 const tableOwners = instance.table('owners');
@@ -15,20 +15,25 @@ const familyOwnersGeneral = tableOwners.family('general');
 
 
 new Promise((resolve, reject) => {
+    console.log("Checking if repos table exists...");
     return tableRepos.exists((error, exists) => {
         if(error) return reject(error);
         return resolve(exists);
     })
 }).then(tableReposExists => {
     if(tableReposExists){
+      console.log("Repos table exists");
         return Promise.resolve([]);
     }
+    console.log("Creating repos table");
     return tableRepos.create();
 }).then(data => {
     const tableRepos = data[0];
     const apiResponse = data[1];
 
+    console.log("Repos table created.");
     return new Promise((resolve, reject) => {
+      console.log("Checking if owners table exists...");
         return tableOwners.exists((error, exists) => {
             if(error) return reject(error);
             return resolve(exists);
@@ -36,14 +41,18 @@ new Promise((resolve, reject) => {
     })
 }).then(tableOwnersExists => {
     if(tableOwnersExists){
+        console.log("Owners table exists");
         return Promise.resolve([])
     }
+    console.log("Creating owners table");
     return tableOwners.create();
 }).then(data => {
     const tableOwners = data[0];
     const apiResponse = data[1];
 
+    console.log("Owners table created.");
     return new Promise((resolve, reject) => {
+        console.log("Checking if repo general family exists...");
         return familyReposGeneral.exists((error, exists) => {
             if(error) return reject(error);
             return resolve(exists);
@@ -51,14 +60,18 @@ new Promise((resolve, reject) => {
     })
 }).then(familyReposGeneralExists => {
     if(familyReposGeneralExists){
+        console.log("Repos general family exists");
         return Promise.resolve([]);
     }
+    console.log("Creating repos general family...");
     return familyReposGeneral.create();
 }).then(data => {
     const family = data[0];
     const apiResponse = data[1];
 
+    console.log("Repos general family created.");
     return new Promise((resolve, reject) => {
+      console.log("Checking if repos social family exists...");
         return familyReposSocial.exists((error, exists) => {
             if(error) return reject(error);
             return resolve(exists);
@@ -66,14 +79,18 @@ new Promise((resolve, reject) => {
     })
 }).then(familyReposSocialExists => {
     if(familyReposSocialExists){
+        console.log("Repos social family exists");
         return Promise.resolve([]);
     }
+    console.log("Creating repos social family...");
     return familyReposSocial.create();
 }).then(data => {
     const family = data[0];
     const apiResponse = data[1];
 
+    console.log("Repos social family created.");
     return new Promise((resolve, reject) => {
+        console.log("Checking if owners general family exists...");
         return familyOwnersGeneral.exists((error, exists) => {
             if(error) return reject(error);
             return resolve(exists);
@@ -81,10 +98,13 @@ new Promise((resolve, reject) => {
     })
 }).then(familyOwnersGeneralExists => {
     if(familyOwnersGeneralExists){
+      console.log("Owners general family exists");
         return Promise.resolve([]);
     }
+    console.log("Creating owners general family...");
     return familyOwnersGeneral.create();
 }).then(data => {
     const family = data[0];
     const apiResponse = data[1];
+    console.log("Owners general family created.");
 }).catch(console.log);
---
+++ save-data-to-bigtable.js
@@ -0,0 +1,102 @@
+const cmdLineArgs = require('command-line-args');
+const request = require('request');
+const _ = require('lodash');
+const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient;
+const MONGO_URL = 'mongodb://localhost:27017/bigtable';
+
+let db, mappedReposData, mappedOwnersData;
+
+const Bigtable = require('@google-cloud/bigtable');
+
+const bigtable = new Bigtable({
+    projectId: 'gridy-github',
+    keyFilename: 'xD.json'
+});
+const instance = bigtable.instance('gridy-github');
+
+const tableRepos = instance.table('repos');
+const tableOwners = instance.table('owners');
+
+const cmdLineArgsConf = require('./cmdLineConf');
+
+return MongoClient.connect(MONGO_URL)
+	.then((bigtableDB) => {
+		db = bigtableDB
+		return db.collection('repos').find().toArray();
+	})
+	.then((allReposData) => {
+		return allReposData.map(element => {
+			return {
+				key: element._id.toString(),
+				data: {
+					general: {
+						name: element.name,
+						size: element.size
+						language: element.language
+					},
+					social:  {
+						issues: element.issues,
+						stars: element.stars,
+						watchers: element.watchers,
+						score: element.score
+					}
+				}
+			}
+		});
+	})
+	.then((mRD) => {
+		mappedReposData = mRD;
+
+		console.log(mappedReposData);
+
+		return new Promise((resolve, reject) => {
+			return tableRepos.insert(mappedReposData, function(err) {
+				if(!err) {
+					return resolve('!!!!!!');
+				}
+				return reject(err);
+			});
+		})
+	})
+	.then(() => {
+		return db.collection('owners').find().toArray();
+	})
+	.then((allOwnersData) => {
+		return allOwnersData.map(element => {
+			return {
+				key: element._id,
+				data: {
+					general: {
+						login: element.login,
+						url: element.url,
+						type: element.type,
+						public_repos: element.public_repos,
+						public_gists: element.public_gists,
+						followers: element.followers,
+						following: element.following,
+						created_at: element.created_at
+					}
+				}
+			}
+		});
+	})
+	.then((mOD) => {
+		mappedOwnersData = mOD;
+
+		return new Promise((resolve, reject) => {
+			return tableOwners.insert(mappedOwnersData, function(err) {
+  			if (!err) {
+    			return resolve();
+  			}
+				return reject(err);
+			});
+		})
+	})
+	.then(xd => {
+		console.log(xd);
+	})
+	.catch(error => {
+		console.log(error);
+		return db.close();
+	})
